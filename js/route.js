ad25cbfcbe3649499267746b2f72545692e34999
김지은 <jxixeun@gmail.com>
2023-03-16 17:47:48 +0900

    :rocket: [Feat] 출발지 검색창에서 키워드 입력받고 카카오에서 장소 정보 가져오기

--- js/route.js
+++ js/route.js
@@ -1,36 +1,38 @@
-//index page 로딩 후 전국의 시도 설정
-let serviceKey = "09a53d4cf09cbe89a1f8ed92cb9a32d8";
-let areaUrl =
-  "https://dapi.kakao.com/v2/local/search/keyword.json?page=1&size=5&sort=accuracy&query=신림역";
-
-// 비동기
-let prom = fetch(areaUrl, {
-  method: "GET",
-  headers: {
-    Authorization: "KakaoAK " + serviceKey,
-  },
-})
-  .then((response) => response.json())
-  .then((data) => console.log(data));
-
 // 검색 버튼을 누르면
 // 지역, 유형, 검색어 얻기
 // 위 데이터를 가지고 공공데이터에 요청
 // 받은 데이터를 이용하여 화면 구성
 document.getElementById("btn-search").addEventListener("click", function () {
-  let searchUrl = `https://apis.data.go.kr/B551011/KorService1/searchKeyword1?serviceKey=yEDlTnRgLJ6TyA%2B5%2FJVGel5GPAULuQKSnfZzzxUgCdlvG%2BfnI8wYyOfYvmMZMjpFHF%2FlhvqHyavoqE5eWqqjgw%3D%3D&numOfRows=10&pageNo=1&MobileOS=ETC&MobileApp=AppTest&_type=json&listYN=Y&arrange=A`;
-  let area = document.getElementById("search-area").value;
-  let contentTypeId = document.getElementById("search-content-id").value;
-  let keyword = document.getElementById("search-keyword").value;
-  if (parseInt(area)) searchUrl += `&areaCode=${area}`;
-  if (parseInt(contentTypeId)) contentTypeId += `&contentTypeId=${contentTypeId}`;
+  //index page 로딩 후 전국의 시도 설정
+  let serviceKey = "09a53d4cf09cbe89a1f8ed92cb9a32d8";
+  let searchUrl =
+    "https://dapi.kakao.com/v2/local/search/keyword.json?page=1&size=5&sort=accuracy&query=";
+  let keyword = document.getElementById("search-departures").value;
   if (!keyword) {
     alert("검색어 입력 필수!!");
     return;
   } else {
-    searchUrl += `&keyword=${keyword}`;
+    searchUrl += `${keyword}`;
   }
-  fetch(searchUrl)
+
+  fetch(searchUrl, {
+    method: "GET",
+    headers: {
+      Authorization: "KakaoAK " + serviceKey,
+    },
+  })
     .then((response) => response.json())
     .then((data) => makeList(data));
 });
+
+function makeList(data) {
+  console.log(data);
+  let trips = data.documents;
+  console.log(trips);
+  let tripList = ``;
+  trips.forEach((area) => {
+    tripList += `<li>${area.place_name}</li>`;
+  });
+
+  document.getElementById("kakaolist").innerHTML = tripList;
+}
