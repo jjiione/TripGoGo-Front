cc8e2eb1ebbb6f84e7f564dc348faddea11f7d38
JeongJiWon <jiwon7164@naver.com>
2023-03-17 15:33:06 +0900

    :memo: [Docs] 마커 이미지 변경

--- js/info.js
+++ js/info.js
@@ -48,8 +48,15 @@ let areaUrl = "https://apis.data.go.kr/B551011/KorService1/areaCode1?serviceKey=
 
 // 카테고리로 검색
 
+var imageSrc = '../images/location-dot-solid.png', // 마커이미지의 주소입니다    
+    imageSize = new kakao.maps.Size(20, 30), // 마커이미지의 크기입니다
+    imageOption = {offset: new kakao.maps.Point(27, 69)}; // 마커이미지의 옵션입니다. 마커의 좌표와 일치시킬 이미지 안에서의 좌표를 설정합니다.
+
+var markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
 // var map = new kakao.maps.Map(mapContainer, mapOption);
 document.getElementById("btn-search").addEventListener("click", () => {
+  // window.location.reload();
   let category = document.getElementById("search-type").value;
 
   var infowindow = new kakao.maps.InfoWindow({zIndex:1});
@@ -63,7 +70,7 @@ ps.categorySearch(category, placesSearchCB, {useMapBounds:true});
 
 // 키워드 검색 완료 시 호출되는 콜백함수 입니다
 function placesSearchCB (data, status, pagination) {
-    if (status === kakao.maps.services.Status.OK) {
+  if (status === kakao.maps.services.Status.OK) {
         for (var i=0; i<data.length; i++) {
             displayMarker(data[i]);    
         }       
@@ -75,7 +82,8 @@ function displayMarker(place) {
     // 마커를 생성하고 지도에 표시합니다
     var marker = new kakao.maps.Marker({
         map: map,
-        position: new kakao.maps.LatLng(place.y, place.x) 
+      position: new kakao.maps.LatLng(place.y, place.x),
+        image: markerImage
     });
 
     // 마커에 클릭이벤트를 등록합니다
