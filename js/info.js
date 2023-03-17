ad8374eabaa9076f3f5fac5e6ed72fdcf0c57514
JeongJiWon <jiwon7164@naver.com>
2023-03-17 12:43:46 +0900

    :lipstick: [Feat] 카카오 맵 설정 js파일로 바꿈

--- js/info.js
+++ js/info.js
@@ -5,37 +5,19 @@
 // 지역, 유형, 검색어 얻기
 // 위 데이터를 가지고 공공데이터에 요청
 // 받은 데이터를 이용하여 화면 구성
-document.getElementById("btn-search").addEventListener("click", function () {
-  //index page 로딩 후 전국의 시도 설정
-  let serviceKey = "09a53d4cf09cbe89a1f8ed92cb9a32d8";
-  let searchUrl =
-    "https://dapi.kakao.com/v2/local/search/category.json?category_group_code=";
-  let keyword = document.getElementById("search-departures").value;
-  if (!keyword) {
-    alert("검색어 입력 필수!!");
-    return;
-  } else {
-    searchUrl += `${keyword}`;
-  }
 
-  fetch(searchUrl, {
-    method: "GET",
-    headers: {
-      Authorization: "KakaoAK " + serviceKey,
-    },
-  })
-    .then((response) => response.json())
-    .then((data) => makeList(data));
-});
 
-function makeList(data) {
-  console.log(data);
-  let trips = data.documents;
-  console.log(trips);
-  let tripList = ``;
-  trips.forEach((area) => {
-    tripList += `<li>${area.place_name}</li>`;
-  });
+var mapContainer = document.getElementById('map'), // 지도를 표시할 div 
+    mapOption = { 
+        center: new kakao.maps.LatLng(37.5013068, 127.0396597), // 지도의 중심좌표
+        level: 8 // 지도의 확대 레벨
+    };
 
-  document.getElementById("kakaolist").innerHTML = tripList;
-}
+// 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
+var map = new kakao.maps.Map(mapContainer, mapOption);
+
+fetch("../json/korea-administrative-district.json")
+.then(response => {
+  return response.json();
+})
+.then(jsondata => console.log(jsondata));
